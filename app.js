8070f455aa2d643447059cc42d5b7006e0fe77b0
João Pedro Israel <juanpeterisrael@gmail.com>
2023-05-04 18:41:11 -0400

    The hell update - Helmet kinda works now - I hate League of Legends

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
@@ -6,6 +10,7 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
 const mongoSanitize = require("express-mongo-sanitize")
+const helmet = require('helmet')
 
 const ExpressError = require('./utils/ExpressError')
 const passport = require('passport')
@@ -16,10 +21,17 @@ const usersRoutes = require('./routes/users')
 const campgroundsRoutes = require('./routes/campgrounds')
 const reviewsRoutes = require('./routes/reviews');
 
+const dbUrl = process.env.DB_URL
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+
+// mongoose.connect(dbUrl, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'Connection Error:'))
@@ -53,6 +65,56 @@ const sessionConfig = {
 
 app.use(session(sessionConfig))
 app.use(flash())
+
+app.use(helmet())
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net/",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://api.mapbox.com/mapbox-gl-js/"
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/dcfw6n4ve/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+              "https://picsum.photos/id/",
+              "https://fastly.picsum.photos/",
+              "https://images.unsplash.com/"
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+      },
+  })
+);
 
 app.use(passport.initialize())
 app.use(passport.session())
